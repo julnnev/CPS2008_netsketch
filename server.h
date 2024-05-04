1566a3c8a570ea645fa061280d38e267c14712d8
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-04 14:33:37 +0200

    Update server.h
    Data structures defined

--- server.h
+++ server.h
@@ -11,18 +11,40 @@
 #include <arpa/inet.h>
 #include <netdb.h>
 #include <string.h>
+#include "draw.h"
 #include <fcntl.h>
 #include <pthread.h>
 #include <string>
 #include <poll.h>
 #include "connect_server.h"
 #include "connect_server.cpp"
+#include <ctime>
 
 #define PORT 5001
 #define MAXMSG  512
 
 class server {
 
+};
+
+struct Command{
+    Draw draw;
+    int commandID;
+    std::string author; //nickname/username of author
+    bool show;
+};
+
+struct userData{
+    std::string username;
+    time_t lastLogged;
+    std::vector<Command> authored;
+};
+
+struct ServerState{
+    int clientsConnected = 0;
+    std::vector<Command> commands;
+    std::unordered_map <int, std::string> socketsToUsernames; //maps socket file descriptor to username
+    std::unordered_map <std::string, userData> namesToData; //maps user data to username
 };
 
 
