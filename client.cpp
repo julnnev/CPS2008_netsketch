45cd8f1443e8c2098c82be1bb7a1ad81773e4e3b
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-04 14:34:14 +0200

    Update client.cpp
    fprintf instead of perror

--- client.cpp
+++ client.cpp
@@ -9,29 +9,23 @@ int main(int argc, char *argv[])
     std::string nickname;
     struct sockaddr_in serv_addr;
     struct hostent *server;
-
     char buffer[256];
-
     // Make sure that server name and port are available in command line arguments
     if (argc < 3) {
         fprintf(stderr, "usage %s : hostname port nickname\n", argv[0]);
         exit(0);
     }
-
     // Get port number
     portno = atoi(argv[2]);
-
-    // Populate nickname
+    //fprintf(stdout, argv[3]);
     nickname = argv[3];
-    // fprintf(stdout, argv[3]);
-
     // Create a socket, similar to server
     if ((sockfd = socket(AF_INET, SOCK_STREAM, 0)) < 0)
     {
-        perror("ERROR opening socket");
+        strerror(sockfd);
+        fprintf(stderr,"ERROR opening socket");
         exit(1);
     }
-
     // Get server name
     // Given string representation of ip address, get the host by name
     if ((server = gethostbyname(argv[1])) == NULL) {
@@ -39,7 +33,6 @@ int main(int argc, char *argv[])
         close(sockfd);
         exit(0);
     }
-
     // Populate serv_addr structure
     memset(&serv_addr, 0, sizeof(serv_addr));
     serv_addr.sin_family = AF_INET; //ipv4
@@ -52,7 +45,7 @@ int main(int argc, char *argv[])
     // Connect to the server
     if (connect(sockfd, (struct sockaddr *) &serv_addr, sizeof(serv_addr)) < 0)
     {
-        perror("ERROR connecting");
+        fprintf(stderr,"ERROR connecting");
         close(sockfd);
         exit(1);
     }
@@ -62,96 +55,53 @@ int main(int argc, char *argv[])
 //        printf("%s\n", buffer);
 //    }// else return message that client connected!
 
-   // while(true){
-        // Ask for a message from the user, if connected successfully
-        printf("Please enter the message: ");
-        memset(buffer, 0, 256);
-        //fgets(buffer, 255, stdin); //place message in buffer
+    // while(true){
+    // Ask for a message from the user, if connected successfully
+    printf("Please enter the message: ");
+    memset(buffer, 0, 256);
+    fgets(buffer, 255, stdin); //place message in buffer
 
-        //snprintf(buffer, "%s", serialized_data.c_str());
+    //snprintf(buffer, "%s", serialized_data.c_str());
 
-        // Replace newline with null terminator
-
-    char *newline = strchr(buffer, '\n');
-    if (newline != nullptr) {
-        *newline = '\0';
-    }
-
-        // check for exit
-        // !! important to close socket server side
-        if (strcmp(buffer, "exit") == 0) {
-            printf("Exiting...\n");
-            close(sockfd);
-            exit(0);
-        }
-
-    // check for help but don't send to server
-    if (strcmp(buffer, "help") == 0) {
-        printf("NetSketch: A Collaborative Whiteboard\n"
-               "\n"
-               "help - Lists all available commands and their usage.\n"
-               "\n"
-               "tool {line | rectangle | circle | text} - Selects a tool for drawing.\n"
-               "\n"
-               "colour {RGB} - Sets the drawing colour using RGB values.\n"
-               "\n"
-               "draw {parameters} - Executes the drawing of the selected shape on the canvas. For text, it requires coordinates and the text string. \n"
-               "\n"
-               "list {all | line | rectangle | circle | text} {all | mine} - Displays issued draw commands in the console.\n"
-               "\n"
-               "select {none | ID} - Selects an existing draw command to be modified by a subsequent draw command.\n"
-               "\n"
-               "delete {ID} - Deletes the draw command with the specified ID.\n"
-               "\n"
-               "undo - Reverts your last draw action.\n"
-               "\n"
-               "clear {all | mine} - Clears the canvas. Using the argument ‘mine‘ clears only the draw commands issued by you; without it, the entire canvas is cleared.\n"
-               "\n"
-               "show {all | mine} - Controls what is displayed on the your canvas. Specifying ‘mine‘ will display only the commands issued by you.\n"
-               "\n"
-               "exit - Disconnects and exits the application.\n");
-
-    }
-
-    Connect connect_request;
-    connect_request.username = nickname;
-    connect_request.success = false;
-    std::ostringstream oss;
-    // Create a BinaryOutputArchive object bound to oss
-    cereal::PortableBinaryOutputArchive archive(oss);
-    archive(connect_request);
-    std::string serialized_data = oss.str();
+//    Connect connect_request;
+//    connect_request.username = nickname;
+//    connect_request.success = false;
+//    std::ostringstream oss;
+//    // Create a BinaryOutputArchive object bound to oss
+//    cereal::PortableBinaryOutputArchive archive(oss);
+//    archive(connect_request);
+//    std::string serialized_data = oss.str();
     //serialise, send to server and check if given nickname valid (ie. unique)
 
     // then proceed to accept instructions after printing success, otherwise exit
 
-        // Send message to the server
-        //if ((n = write(sockfd, serialized_data.data(), strlen(serialized_data.size()))) < 0)
-        if ((n = write(sockfd, buffer, strlen(buffer))) < 0)
-        {
-            perror("ERROR writing to socket");
-            close(sockfd);
-            exit(1);
-        }
+    // Send message to the server
+    //if ((n = write(sockfd, serialized_data.data(), strlen(serialized_data.size()))) < 0)
+    if ((n = write(sockfd, buffer, strlen(buffer))) < 0)
+    {
+        fprintf(stderr,"ERROR writing to socket");
+        close(sockfd);
+        exit(1);
+    }
 
-        // Read response from server response
-        memset(buffer, 0, 256);
-        if ((n = read(sockfd, buffer, 255)) < 0)
-        {
-            perror("ERROR reading from socket");
-            close(sockfd);
-            exit(1);
-        }
+    // Read response from server response
+    memset(buffer, 0, 256);
+    if ((n = read(sockfd, buffer, 255)) < 0)
+    {
+        fprintf(stderr,"ERROR reading from socket");
+        close(sockfd);
+        exit(1);
+    }
 
-        printf("%s\n", buffer);
+    printf("%s\n", buffer);
     //}
 
     return 0;
 }
 
 // no: g++ -o client  client.cpp
-
 // cd build
 //gmake -j 8
 // ./client 127.0.0.1 5001 ju
+
 
