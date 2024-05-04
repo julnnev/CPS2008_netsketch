cff81e922166b0acee628605b7c64c065d4ae728
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-04 12:10:51 +0200

    client basis

---
+++ client.h
@@ -0,0 +1,24 @@
+#ifndef NETSKETCH_CLIENT_H
+#define NETSKETCH_CLIENT_H
+
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <unistd.h>
+#include <string.h>
+#include "connect_server.h"
+#include "connect_server.cpp"
+
+
+class client {
+
+};
+
+
+#endif //NETSKETCH_CLIENT_H
+
+// input, read from server, write to server, draw (main function due to glfw)
