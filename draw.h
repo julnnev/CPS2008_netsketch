2c1393bbae165ba231247fc3bfcf067e3331fd64
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-04 12:23:17 +0200

    Create draw.h
    Stores structs related to shapes and drawing command

---
+++ draw.h
@@ -0,0 +1,46 @@
+//
+// Created by Julianne Vella on 04/05/2024.
+//
+
+#ifndef NETSKETCH_DRAW_H
+#define NETSKETCH_DRAW_H
+
+struct CircleShape {
+    std::string name = "circle";
+    int x;
+    int y;
+    float radius;
+};
+
+struct RectangleShape  {
+    std::string name = "rectangle";
+    int topLeftX;
+    int topLeftY;
+    int bottomRightX;
+    int bottomRightY;
+};
+
+struct LineShape{
+    std::string name = "line";
+    int startX{};
+    int startY{};
+    int endX{};
+    int endY{};
+};
+
+struct TextShape{
+    std::string name = "text";
+    int x;
+    int y;
+    std::string text;
+};
+
+using Item = std::variant<CircleShape, RectangleShape, LineShape, TextShape>;
+
+struct Command {
+    Item item;
+    std::tuple<int, int, int> RGBColour;
+};
+
+
+#endif //NETSKETCH_DRAW_H
