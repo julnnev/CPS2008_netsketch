8fad5c092469e0fba20306876009986c4f9f51e3
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-04 12:11:43 +0200

    Creation of data structure to check if connection is allowed

---
+++ connect_server.cpp
@@ -0,0 +1,8 @@
+//
+// Created by Julianne Vella on 29/04/2024.
+//
+
+#include <string>
+#include "connect_server.h"
+
+
---
+++ connect_server.h
@@ -0,0 +1,21 @@
+//
+// Created by Julianne Vella on 29/04/2024.
+//
+
+#ifndef NETSKETCH_CONNECT_SERVER_H
+#define NETSKETCH_CONNECT_SERVER_H
+#include <cereal/types/string.hpp>
+class Connect { //change to struct
+public:
+    std::string username;
+    bool success;
+
+    // Serialization function
+    template<class Archive>
+    void serialize(Archive & archive) {
+        archive(username, success);
+    }
+};
+
+
+#endif //NETSKETCH_CONNECT_SERVER_H
