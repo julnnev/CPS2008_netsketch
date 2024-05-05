e5f2b4a4ded1b26fb69930256788394b59a4f12b
Julianne Vella <54096586+julnnev@users.noreply.github.com>
2024-05-05 13:07:58 +0200

    Global variables

---
+++ global_client.cpp
@@ -0,0 +1,7 @@
+//
+// Created by Julianne Vella on 05/05/2024.
+//
+
+#include "global_client.h"
+
+std::vector<Draw> drawList;
---
+++ global_client.h
@@ -0,0 +1,18 @@
+//
+// Created by Julianne Vella on 05/05/2024.
+//
+
+#ifndef NETSKETCH_GLOBAL_CLIENT_H
+#define NETSKETCH_GLOBAL_CLIENT_H
+
+#include <iostream>
+#include "draw.h"
+
+extern std::vector<Draw> drawList;
+
+class global_client {
+
+};
+
+
+#endif //NETSKETCH_GLOBAL_CLIENT_H
